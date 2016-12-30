29feceaa4ca768100ee94702fb74b3dc23fe326f
yang <lyxs916@163.com>
2016-12-30 14:10:40 +0800

    1

--- app/src/main/java/lyxs916/addressselect/MainActivity.java
+++ app/src/main/java/lyxs916/addressselect/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
 
-
                 DialogCitySelect dialogCitySelect = new DialogCitySelect(MainActivity.this);
                 dialogCitySelect.setCallBack(new DialogCitySelect.CallBack() {
                     @Override
